d83fac607103c77c564f77439c27b95e2039bc64
tladson2016 <ladsont10@gmail.com>
2017-08-15 08:05:02 -0400

    update

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ console.log(userName);
 }
 
 function userid_validation(num1,num2){
-    var userName = document.getElementsByName('userid')[0].value;
+    var userName = document.getElementsByName('userid').value;
 
   if (!userName || userName == ''){
     console.log('Please Enter Your User Name.');
   }
 
-  if (userName => 5 || userName <= 12) {
+  if (userName >= 5 && userName <= 12) {
       console.log("Your User name is accepted!");
       return true;
   }
@@ -28,8 +28,8 @@ function passId() {
 
 
 function passid_validation(num1,num2) {
-    var password = document.getElementsByName('passid')[0];
-  if (!password.value || password.value == ''){
+    var password = document.getElementsByName('passid')[0].value;
+  if (!password || password == ''){
     console.log('User password should not be empty!')
   } else {
     console.log('Your password is invalid.')
@@ -55,7 +55,10 @@ function validateEmail() {
    var elength = userEmail.length;
    var dot = userEmail.indexOf('.');
    var atSym = userEmail.indexOf('@');
-  if (!userEmail == ){
 
+  if (userEmail == ''){
+    console.log('You have entered an invalid email format!');
+    return false;
   }
+
  }
